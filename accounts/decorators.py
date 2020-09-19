b6427bc71bd6c4e8dc1e9a5340616e8af3301bb6
Javokhirbek <jurakhodjayev2000@mail.ru>
2020-09-19 09:14:57 +0500

    Update decorators.py

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
-
+# decorators
 def unauthenticated_user(view_func):
 	def wrapper_func(request, *args, **kwargs):
 		if request.user.is_authenticated:
